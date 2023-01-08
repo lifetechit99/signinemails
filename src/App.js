8635e9696bb3c2ccf766e23b2f3a0d3366251e75
lifetechit99 <lifetechit99@gmail.com>
2023-01-08 14:53:31 +0700

    a

--- src/App.js
+++ src/App.js
@@ -23,7 +23,9 @@ class App extends React.Component {
         <p>pass <input name="pass" value={this.state.pass} onChange={this.onChange.bind(this)} /> <BtnCopy value={this.state.pass} /></p>
         <p> <a href="https://vt.tiktok.com/ZS8Mv8bPv/" target="_blank">Link An</a></p>
         <p> <a href="https://vt.tiktok.com/ZS8Mv1qy5/" target="_blank">Link Quynh</a></p>
-        <p> <a target="_blank">Link Luan</a></p>
+        <p> <a href="https://vt.tiktok.com/ZS8r3TkSB/" target="_blank">Link K</a></p>
+        <p> <a href="https://play.google.com/store/apps/details?id=com.dong.autonexttiktok" target="_blank">Auto Next tiktok</a></p>
+        
         <p> <a href="https://docs.google.com/spreadsheets/d/1ZpgerK-wuHxGTW4k_MBVqxDmkwv3qhd6QmR65bLlbYo/edit#gid=0" target="_blank">File tài khoản</a></p>
       </div>
     );
